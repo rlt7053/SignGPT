a81e49bed7fbb5872ee0b630ecb8c0b650cfaf48
Loam Shin <loamshin@acd41229045323768730571.wireless.rit.edu>
2023-03-05 01:02:14 -0500

    sidebar updates

--- signgpt/src/components/Sidebar.js
+++ signgpt/src/components/Sidebar.js
@@ -1,9 +1,39 @@
+import Nav from "react-bootstrap/Nav";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+
+
+
+
+
 function Sidebar() {
   return (
-    <div className="Sidebar">
-      This is the sidebar!
+
+
+    <Nav defaultActiveKey="/home" className="flex end">
+      <div class = "fixed-sidebar">
+
+
+
+      <button class="button newBtn">
+      <FontAwesomeIcon icon={(faCoffee)} />
+        New Chat</button>
+      <ul>
+        <li><Nav.Link href="/home">SignGPT</Nav.Link></li>
+        <li><Nav.Link eventKey="link-1">Upgrade to PLUS</Nav.Link></li>
+        <li> <Nav.Link eventKey="link-2">Dark Mode</Nav.Link></li>
+        <li><Nav.Link eventKey="link-3">Updates & FAQ</Nav.Link></li>
+        <li><Nav.Link eventKey="link-4">Log Out</Nav.Link></li>
+
+      </ul>
+
     </div>
+
+    </Nav>
+
   );
 }
 
 export default Sidebar;
+
