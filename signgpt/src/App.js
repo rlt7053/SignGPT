88ebd90b02fefeb5402af15a11590ae4ab1f9f55
Rachel Taylor <rlt7053@rit.edu>
2023-03-05 00:47:38 -0500

    add a layout for Chat

--- signgpt/src/App.js
+++ signgpt/src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import Chat from './components/Chat';
 import Sidebar from './components/Sidebar';
-import Signbar from './components/SignBar';
+import Signbar from './components/Signbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <Sidebar />
-      <Chat />
-      <Signbar />
+      <div className="interactive">
+        <Chat />
+        <Signbar />
+      </div>
     </div>
   );
 }
